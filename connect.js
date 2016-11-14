82e60eafa913d08f91400f58172dac03ed417d1a
Tiago Cruz <onisciente@gmail.com>
2016-11-14 17:02:51 +0000

    Add subscribe.html.

---
+++ connect.js
@@ -0,0 +1,3 @@
+// Or you can simply use a connection uri
+global.Sequelize = new Sequelize('mysql://bcba31cf84da31:f51d57ee@us-cdbr-iron-east-04.cleardb.net/heroku_fed2d83060b70ed');
+/*global Sequelize*/
